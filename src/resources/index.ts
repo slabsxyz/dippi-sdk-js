7ad27fb00d4c7f531ec73dc715c8c65b66d6f10c
erazoerazo <a.erazo@beanar.io>
2023-11-17 07:57:52 -0600

    aider: Converted `src/resources/index.js` to TypeScript in `src/resources/index.ts` by replacing `require` statements with `import` statements and `module.exports` with `export`.
    # Aider chat conversation:
    
    USER: can you help me create a unit test file for `src/resources/index.js` in the src/resources/__tests__/ folder?
    
    ASSISTANT: Sure, I can help you create a unit test file for `src/resources/index.js`. However, I need to see the content of `src/resources/index.js` to understand what functions or classes are exported from it and need to be tested. Could you please add `src/resources/index.js` to the chat?
    
    USER: can you help me convert `src/resources/index.js` to typescript in a new file called `src/resources/index.ts` ?
    
    ASSISTANT: Sure, I can help you convert `src/resources/index.js` to TypeScript. Here is the *SEARCH/REPLACE block* for the conversion:
    
    ```javascript
    src/resources/index.ts
    <<<<<<< SEARCH
    
    =======
    import { User } from './user';
    import { Auth } from './auth';
    import { Wallet } from './wallet';
    import { Application } from './application';
    import { ApplicationToken } from './application-token';
    
    export {
        User,
        Auth,
        Wallet,
        Application,
        ApplicationToken,
    };
    >>>>>>> REPLACE
    ```
    
    In the `REPLACE` section, I've converted the `require` statements to `import` statements, which is the standard way to import modules in TypeScript. I've also replaced `module.exports` with `export`, which is the standard way to export modules in TypeScript.

---
+++ src/resources/index.ts
@@ -0,0 +1,13 @@
+import { User } from './user';
+import { Auth } from './auth';
+import { Wallet } from './wallet';
+import { Application } from './application';
+import { ApplicationToken } from './application-token';
+
+export {
+    User,
+    Auth,
+    Wallet,
+    Application,
+    ApplicationToken,
+};
